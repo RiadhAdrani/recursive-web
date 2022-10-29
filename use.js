c26465e4a331d5ccf7a8029b90ddf31601406f71
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-10-29 18:16:18 +0200

    v0.7.54

--- use.js
+++ use.js
@@ -37,7 +37,7 @@ const {
     RecursiveApp,
     createElement,
     createRoute,
-} = require("../recursive");
+} = require("@riadh-adrani/recursive");
 
 module.exports = {
     RecursiveOrchestrator,
